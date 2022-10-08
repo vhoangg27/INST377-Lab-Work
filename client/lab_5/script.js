b4fac085ce0b41ccf81e9226417f64d75f4fde31
Vivian Hoang <vhoangg27@gmail.com>
2022-10-08 17:34:12 -0400

    update

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -34,8 +34,8 @@ async function mainEvent() { // the async keyword means we can make API requests
       // this is a basic GET request
       // It does not include any of your form values, though
     */
-
-    const results = await fetch('/api/foodServicePG');
+    constfetchQuery = new URLSearchParams(formProps);
+    const results = await fetch(`/api/foodServicePG?${fetchQuery}`);
     /*
    ## Get request with query parameters
 
